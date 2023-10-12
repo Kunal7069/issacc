4bdca2ec2e9762f45b7673fc83bbaac74c4e6e28
Aditya <adityacy.004@gmail.com>
2023-10-12 20:23:51 +0530

    Initial commit

---
+++ src/components/Clubs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Clubs = () => {
+  return (
+    <div className='bg-gray-800'>Clubs</div>
+  )
+}
+
+export default Clubs;
---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import '../App.css'
+const Header = () => {
+  return (
+    <>
+      <header className="flex items-center justify-center bg-slate-500">
+        <div className="">LOGO</div>
+
+        <div className="flex items-end justify-end">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/"> Home </Link>
+              </li>
+              <li>
+                <Link to="/About"> About </Link>
+              </li>
+              <li>
+                <Link to="/Events"> Events </Link>
+              </li>
+              <li>
+                <Link to="/Clubs"> Clubs </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Outlet />
+      </header>
+    </>
+  );
+};
+
+export default Header;
